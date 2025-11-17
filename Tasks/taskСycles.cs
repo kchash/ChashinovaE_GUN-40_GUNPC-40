75755a95b5f2540bc8eabb962ea4b81382e1068a
AirbusParking <167079784+AirbusParking@users.noreply.github.com>
2025-11-17 06:05:59 -0800

    edit homework

--- Tasks/taskСycles.cs
+++ Tasks/taskСycles.cs
@@ -42,7 +42,7 @@ namespace Homework
                 input = Console.ReadLine();
             }
             while (input != password);
-            Console.WriteLine("Пароль верный!");
+            Console.WriteLine("Пароль верный");
         }
         
         
